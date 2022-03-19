53dc6e7d0dd23c0fecdfab2cfad4819cb9c07861
samih damaj <samih_damaj@hotmail.com>
2022-03-19 13:08:16 +0200

    commit

--- lib/modules/feeds/feeds_screen.dart
+++ lib/modules/feeds/feeds_screen.dart
@@ -94,9 +94,7 @@ class FeedsScreen extends StatelessWidget {
                                                   e.value.last)),
                                   ],
                                 )
-                              : SizedBox(
-                                  width: 0,
-                                ),
+                              : _noStoriesWidget(context),
                         ],
                       )
                       // : FirestoreListView<StoryModel>(
@@ -671,6 +669,17 @@ class FeedsScreen extends StatelessWidget {
       ),
     ];
   }
+
+  _noStoriesWidget(BuildContext context) => Container(
+        width: MediaQuery.of(context).size.width * 0.6,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("No stories yet"),
+            Text("Add A Story"),
+          ],
+        ),
+      );
 
   // Future _refreshData() async {
   //   Get.delete<SocialLayoutController>();
