0fb3ba6c8e7a5978e245ac07a7a4df9e7a0d8cc0
samih damaj <samih_damaj@hotmail.com>
2022-05-24 12:56:17 +0300

    chage to listen request

--- lib/modules/notifications_screen/notificationsscreen.dart
+++ lib/modules/notifications_screen/notificationsscreen.dart
@@ -1,4 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutterfire_ui/firestore.dart';
 import 'package:get/get.dart';
 import 'package:lottie/lottie.dart';
 import 'package:social_app/model/friend_request.dart';
@@ -7,88 +9,92 @@ import 'package:social_app/shared/constants.dart';
 import 'package:social_app/shared/styles/colors.dart';
 
 class NotificationsScreen extends StatelessWidget {
+  var friendrequestsQuery = FirebaseFirestore.instance
+      .collection('users')
+      .doc(uId)
+      .collection('receivedfriendrequest')
+      .orderBy('requestDate', descending: true)
+      .withConverter<FriendRequest>(
+        fromFirestore: (snapshot, _) =>
+            FriendRequest.fromJson(snapshot.data()!),
+        toFirestore: (request, _) => request.toJson(),
+      );
+
   var controller = Get.put(NotificationController());
   @override
   Widget build(BuildContext context) {
-    return GetBuilder<NotificationController>(
-        init: Get.find<NotificationController>(),
-        builder: (notificationController) {
-          return Scaffold(
-            body: notificationController
-                        .listofreceivedRequestTodisplay.length ==
-                    0
-                ? Container(
-                    width: double.infinity,
-                    child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Lottie.asset('assets/lottie/notifications.json',
-                              width: 150, height: 150),
-                          SizedBox(
-                            height: 10,
-                          ),
-                          Text("Your Notifications",
-                              style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.grey)),
-                          Text("No Notifications to show at the moment",
-                              style: TextStyle(color: Colors.grey)),
-                          Text("Check back Later",
-                              style: TextStyle(color: Colors.grey)),
-                        ]),
-                  )
-                : Container(
-                    padding: EdgeInsets.all(10),
-                    width: double.infinity,
-                    child: Column(
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              padding: EdgeInsets.all(12),
+              height: MediaQuery.of(context).size.height * 0.4,
+              child: FirestoreListView<FriendRequest>(
+                  // reverse: true,
+                  //    physics: NeverScrollableScrollPhysics(),
+                  // shrinkWrap: true,
+                  pageSize: 5,
+                  query: friendrequestsQuery,
+                  // loadingBuilder: (context) => Center(
+                  //       child: CircularProgressIndicator(),
+                  //     ),
+                  errorBuilder: (context, error, stackTrace) =>
+                      Text('Something went wrong! ${error} - ${stackTrace}'),
+                  itemBuilder: (context, snapshot) {
+                    FriendRequest model;
+
+                    if (snapshot.isBlank!) {
+                      print('snapshot blank');
+
+                      print(snapshot.data().name);
+                      return Container();
+                    } else {
+                      model = snapshot.data();
+                      print('snapshot data');
+                    }
+
+                    return Column(
                       children: [
-                        Container(
-                          height: MediaQuery.of(context).size.height * 0.35,
-                          child: ListView(
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 12),
+                          child: Row(
                             children: [
-                              Row(
-                                children: [
-                                  Text(
-                                    "Friend requests",
-                                    style:
-                                        TextStyle(fontWeight: FontWeight.bold),
-                                  ),
-                                  SizedBox(
-                                    width: 5,
-                                  ),
-                                  Text(
-                                    notificationController
-                                        .listofreceivedRequestTodisplay.length
-                                        .toString(),
-                                    style: TextStyle(
-                                        color: Colors.red,
-                                        fontWeight: FontWeight.bold,
-                                        fontSize: 16),
-                                  ),
-                                  Spacer(),
-                                  Text(
-                                    "See all",
-                                    style: TextStyle(color: defaultColor),
-                                  ),
-                                ],
+                              Text(
+                                "Friend requests",
+                                style: TextStyle(fontWeight: FontWeight.bold),
                               ),
                               SizedBox(
-                                height: 10,
+                                width: 5,
                               ),
-                              ...notificationController
-                                  .listofreceivedRequestTodisplay
-                                  .map((e) => _buildRequestItem(e, context)),
+                              Text(
+                                '${controller.listofreceivedfriendrequests.length}',
+                                style: TextStyle(
+                                    color: Colors.red,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 16),
+                              ),
+                              Spacer(),
+                              Text(
+                                "See all",
+                                style: TextStyle(color: defaultColor),
+                              ),
                             ],
                           ),
                         ),
+                        _buildRequestItem(model, context, controller),
                       ],
-                    ),
-                  ),
-          );
-        });
+                    );
+                  }),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 
-  _buildRequestItem(FriendRequest model, BuildContext context) {
+  _buildRequestItem(FriendRequest model, BuildContext context,
+      NotificationController controller) {
     return Container(
       height: MediaQuery.of(context).size.height * .15,
       child: Row(
@@ -134,7 +140,8 @@ class NotificationsScreen extends StatelessWidget {
                             borderRadius: BorderRadius.circular(8)),
                         color: defaultColor.shade800,
                         onPressed: () {
-                          controller.confirmRequest(myId: uId.toString(),
+                          controller.confirmRequest(
+                              myId: uId.toString(),
                               user_requestId: model.requestId.toString());
                         },
                         child: Text(
